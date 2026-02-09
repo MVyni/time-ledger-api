43d6ccd2943fd5a129dc3980c95f7664c646f095
Marcus Vynicius <viniciusfla1999@gmail.com>
2026-02-09 21:34:42 +0000

    github actions unit

--- src/http/controllers/work-entries/fetch-history.test.ts
+++ src/http/controllers/work-entries/fetch-history.test.ts
@@ -13,7 +13,7 @@ describe('Fetch Work Entries History (e2e)', () => {
   it('should be able to fetch work entries history', async () => {
     const { token } = await createAndAuthUser(app)
 
-    const create1 = await request(app)
+    await request(app)
       .post('/workentrie/create')
       .set('Authorization', `Bearer ${token}`)
       .send({
@@ -21,9 +21,8 @@ describe('Fetch Work Entries History (e2e)', () => {
         durationMinutes: 60,
         hourlyRateAtTime: 50,
       })
-    expect(create1.statusCode).toEqual(201)
 
-    const create2 = await request(app)
+    await request(app)
       .post('/workentrie/create')
       .set('Authorization', `Bearer ${token}`)
       .send({
@@ -31,14 +30,12 @@ describe('Fetch Work Entries History (e2e)', () => {
         durationMinutes: 60,
         hourlyRateAtTime: 50,
       })
-    expect(create2.statusCode).toEqual(201)
 
     const response = await request(app)
       .get('/workentrie/history')
       .set('Authorization', `Bearer ${token}`)
       .send()
 
-      console.log("RESPONSEE", response.body)
     expect(response.statusCode).toEqual(200)
     expect(response.body.monthlyHistory).toEqual(
       expect.arrayContaining([
